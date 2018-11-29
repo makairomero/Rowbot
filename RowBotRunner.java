f5718de4b1238b6e2edf59d7960f41c60c37d1c8
Toby <tfranklin0181@myduneland.org>
2018-11-29 12:05:00 -0600

    a

--- RowBotRunner.java
+++ RowBotRunner.java
@@ -7,21 +7,25 @@ public class RowBotRunner
 {
     public static void main(String[] args)
     {
+        int a = 0;
         String same = ""; 
         RowBot maggie = new RowBot();
         
         System.out.println (maggie.getGreeting());
+        while(a == 0){
         Scanner in = new Scanner (System.in);
         String statement = in.nextLine();
         
-        while (!statement.equals("Bye"))
-        {
+        if  (statement.equals("Bye"))
+        {System.out.println("Bye");
+            break;
+        }
             System.out.println (maggie.getResponse(statement, same));
             statement = in.nextLine();
             same = statement;
-        }
         
+        }
         System.out.println("Ok bye.");
+    
     }
-
 }
