d77fd5c953b5dc5bb756e35d0c8ebb31ef0f975f
Evan Ekblaw <eekblaw@myduneland.org>
2018-12-13 12:02:56 -0600

    g

--- RowBot.java
+++ RowBot.java
@@ -228,9 +228,9 @@ public class RowBot
         }
         
         if (num1 != 1 && num1 != 2 && num1 != 3 && num1 != 4 && num1 != 5 
-        && num1 != 6 && num1 != 7 && num1 != 8 && num1 != 9) response = "enter numbers from 0-9";
+        && num1 != 6 && num1 != 7 && num1 != 8 && num1 != 9) response = "Enter numbers from 0-9";
         else if (num2 != 1 && num2 != 2 && num2 != 3 && num2 != 4 && num2 != 5 
-        && num2 != 6 && num2 != 7 && num2 != 8 && num2 != 9) response = "enter numbers from 0-9";
+        && num2 != 6 && num2 != 7 && num2 != 8 && num2 != 9) response = "Enter numbers from 0-9";
         else if (findKeyword(statement, "plus") >= 0
         || findKeyword(statement, "Plus") >= 0
         || findKeyword(statement, "+") >= 0)
